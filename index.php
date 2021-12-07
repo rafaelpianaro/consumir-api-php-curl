5c3cfdf4e50f04ad34a92bea5c60ccd7dfe28642
Rafael <rafaelpianaro@gmail.com>
2021-12-06 22:14:17 -0300

    *echo the results

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Consumir API no PHP com cURL</title>
 </head>
 <body>
     <?php
@@ -13,7 +13,26 @@
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $results = json_decode(curl_exec($ch));
-        var_dump($results);
+        // var_dump($results);
+        foreach($results->results as $result){
+            // var_dump($result);
+            echo "Nome: ".$result->name."</br>";
+            echo "Altura: ".$result->height."</br>";
+            echo "Ano Nascimento: ".$result->birth_year."</br>";
+            if(count($result->films) > 1){
+                echo "Participou em ". count($result->films). " filmes </br>";
+                foreach($result->films as $film){
+                    echo "Listagem de filmes: ".$film."</br>";
+                }
+            }
+            else{
+                echo "Participou em ". count($result->films). " filme </br>";
+                foreach($result->films as $film){
+                    echo "Filme: ".$film."</br>";
+                }
+            }
+            echo "<hr>";
+        }
     ?>
 </body>
 </html>
